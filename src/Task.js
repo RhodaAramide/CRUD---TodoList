3d0c439bf8b8ea0f27c146028af7952ccb5bdca6
RhodaAramide <olabisiabolarin5@gmail.com>
2023-08-11 00:56:38 +0100

    pushed

---
+++ src/Task.js
@@ -0,0 +1,14 @@
+
+
+export const Task = (props) => {
+  return (
+    <div 
+        className="task"
+        style={{ backgroundColor: props.completed ? "red" : "white" }}
+    >
+        <h1>{props.taskName}</h1>
+        <button onClick={() => props.deleteTask(props.id)}> X </button>
+        <button onClick={() => props.completeTask(props.id)}> Complete </button>
+    </div>
+  );
+};
